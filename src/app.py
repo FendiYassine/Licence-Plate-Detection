1efa6ec037260b9f952bc3437f30c56b49b0ab88
FendiYassine <yassine.fendi@insat.ucar.tn>
2023-12-20 13:08:11 +0100

    Fixing bug

--- src/app.py
+++ src/app.py
@@ -2,13 +2,9 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 import subprocess
-import pytesseract
 from PIL import Image
 import pandas as pd
-# import pytesseract
 import easyocr
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Adjust the path accordingly
-
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
@@ -17,11 +13,11 @@ def run_inference(file_path):
     output_dir = 'output'
     os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
     output_path = os.path.join(output_dir, 'temp.jpg')  # Use 'temp.jpg' as the file name
-    temp_txt_path = os.path.join(output_dir, 'labels', 'temp.txt')  # Path to temp.txt
+    temp_txt_path = os.path.join(output_dir, 'result/labels', 'temp.txt')  # Path to temp.txt
 
-    # Remove existing temp.txt file
     if os.path.exists(temp_txt_path):
-        os.remove(temp_txt_path)
+        with open(temp_txt_path, 'w') as f:
+            f.write('')  # Clear the content of temp.txt
 
     command = [
         "python",
@@ -51,59 +47,84 @@ def run_inference(file_path):
         print('Error during inference:', e)
         return {"error": str(e)}
 
-def crop_image(image_path, x1, y1, x2, y2):
-    # Crop the image using PIL
-    print(image_path)
+
+
+def crop_image(image_path, x_center, y_center, width_percent, height_percent):
+    # Open the image using PIL
     image = Image.open(image_path)
-    cropped_image = image.crop((x1, y1, x2, y2))
+    img_width, img_height = image.size
+    Image.MAX_IMAGE_PIXELS = None
+
+    # Convert YOLO bounding box (normalized values) to PIL format (pixel values)
+    left = img_width * (x_center - width_percent / 2.)
+    right = img_width * (x_center + width_percent / 2.)
+    upper = img_height * (y_center - height_percent / 2.)
+    lower = img_height * (y_center + height_percent / 2.)
+
+    # Crop the image
+    cropped_image = image.crop((left, upper, right, lower))
     cropped_image_path = 'output/cropped.jpg'
     cropped_image.save(cropped_image_path)
     return cropped_image_path
 
-def extract_bounding_box_from_yolo_output(output_path, image_width, image_height):
+def extract_bounding_box_from_yolo_output(output_path):
     # Extract bounding box coordinates from YOLOv5 output
     with open(output_path, 'r') as f:
         lines = f.readlines()
-        values = lines[0].split(' ')[1:6]  # Corrected the index range
-        x_center, y_center, width_percent, height_percent, _ = map(float, values)
-
-        # Convert percentages to actual values
-        width = width_percent * image_width
-        height = height_percent * image_height
-
-        return x_center, y_center, width, height
-
-
-        
-
+        values = lines[0].split(' ')[1:5]
+        x_center, y_center, width_percent, height_percent = map(float, values)
+        return x_center, y_center, width_percent, height_percent
 
 def perform_ocr():
     image_path = 'C:\\Users\\torjm\\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\result\\temp.jpg'
-    image = Image.open(image_path)
-    image_width, image_height = image.size
-    x1, y1, width, height = extract_bounding_box_from_yolo_output('C:\\Users\\torjm\\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\result\\labels\\temp.txt', image_width, image_height)
-    print('as you see', x1, y1, width, height)
-    
+    x_center, y_center, width_percent, height_percent = extract_bounding_box_from_yolo_output('C:\\Users\\torjm\\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\result\\labels\\temp.txt')
+
     # Crop the image using bounding box coordinates
-    cropped_image_path = crop_image(image_path, int(x1), int(y1), int(x1 +width), int(y1+ height))
-    
-    print('ena*******', cropped_image_path)
-    
-    reader = easyocr.Reader(['en'])  # You can add more languages if needed
+    cropped_image_path = crop_image(image_path, x_center, y_center, width_percent, height_percent)
+
+    print('Cropped image path:', cropped_image_path)
+
+    # Configure OCR to detect English alphabet and digits
+    reader = easyocr.Reader(['en'])
     result = reader.readtext(cropped_image_path)
     extracted_text = ' '.join([entry[1] for entry in result])
 
     return extracted_text
 
 
-def save_to_excel(extracted_text):  
+def save_to_excel(extracted_text):
     # Save extracted text to an Excel file
+    excel_path = 'C:\\Users\\torjm\\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\extracted_information.xlsx'
 
-    df = pd.DataFrame({'Extracted Information': [extracted_text]})
-    excel_path = 'C:\\Users\\torjm\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\extracted_information.xlsx'
+    # Check if the file exists
+    if os.path.exists(excel_path):
+        # If the file exists, read the existing data
+        df = pd.read_excel(excel_path)
+    else:
+        # If the file does not exist, create a new DataFrame
+        df = pd.DataFrame()
+
+    # Append the new data
+    new_data = pd.DataFrame({'Extracted Information': [extracted_text]})
+    df = pd.concat([df, new_data], ignore_index=True)
+
+    # Write the DataFrame back to the Excel file
     df.to_excel(excel_path, index=False)
+
     return excel_path
 
+
+def clear_temp_txt():
+    temp_txt_path = 'C:\\Users\\torjm\\OneDrive\\Bureau\\licence plate\\license-plate-ocr\\output\\result\\labels\\temp.txt'
+    
+    # Check if the file exists
+    if not os.path.exists(temp_txt_path):
+        # If the file does not exist, create it
+        open(temp_txt_path, 'w').close()
+
+    # Now the file should always exist
+    with open(temp_txt_path, 'w') as f:
+        f.write('')  # Clear the content of temp.txt
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Handle file upload
@@ -124,6 +145,9 @@ def upload_file():
 
     # Save extracted information to an Excel file
     excel_path = save_to_excel(extracted_text)
+
+    # Clear the content of temp.txt
+    clear_temp_txt()
 
     return jsonify({
         'message': 'File uploaded successfully',
